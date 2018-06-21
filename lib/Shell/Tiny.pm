e8340e9b69935531fc1dbe4c772adb7e0095b40e
K <x00.x7f@gmail.com>
2018-06-21 19:48:56 +0900

    Initial commit

---
+++ lib/Shell/Tiny.pm
@@ -0,0 +1,39 @@
+package Shell::Tiny;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Shell::Tiny - It's new $module
+
+=head1 SYNOPSIS
+
+    use Shell::Tiny;
+
+=head1 DESCRIPTION
+
+Shell::Tiny is ...
+
+=head1 LICENSE
+
+Copyright (C) K.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+K E<lt>x00.x7f@gmail.comE<gt>
+
+=cut
+
